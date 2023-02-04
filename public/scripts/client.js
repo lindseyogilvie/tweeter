e02a633b5b628c24b3263cc6e0484d32ea4aaa63
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-02-03 18:05:18 -0800

    Feed shows tweets from youngest to oldest

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,7 +6,7 @@
 
 $(document).ready(function() {
   
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
@@ -15,7 +15,8 @@ $(document).ready(function() {
   const renderTweets = function(tweets) {
     $('.tweet-feed').empty();
     // Iterate through array of tweet objects
-    for (let tweet of tweets) {
+    for (let i = tweets.length - 1; i >= 0; i--) {
+      let tweet = tweets[i];
       // Pass each tweet object into the createTweetElement
       const $tweet = createTweetElement(tweet);
       // Append each tweet element to the .tweet-feed container
@@ -25,25 +26,25 @@ $(document).ready(function() {
 
   const createTweetElement = function(tweet) {
     let $tweet = $(
-    `<article class="tweet">
-      <header>
-        <div>
-          <img src=${tweet.user.avatars}>
-          <span>${tweet.user.name}</span>
-        </div>
-        <span>${tweet.user.handle}</span>
-      </header>
-      <div class="tweet-body">${escape(tweet.content.text)}</div>
-      <footer>
-        <span>${timeago.format(tweet.created_at)}</span>
-        <div>
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
-        </div>
-      </footer>
-    </article>
-    <br>`
+      `<article class="tweet">
+        <header>
+          <div>
+            <img src=${tweet.user.avatars}>
+            <span>${tweet.user.name}</span>
+          </div>
+          <span>${tweet.user.handle}</span>
+        </header>
+        <div class="tweet-body">${escape(tweet.content.text)}</div>
+        <footer>
+          <span>${timeago.format(tweet.created_at)}</span>
+          <div>
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+      <br>`
     );
 
     return $tweet;
@@ -60,32 +61,28 @@ $(document).ready(function() {
         Tweets cannot be empty. Share what's on your mind!
         <i class="fa-solid fa-triangle-exclamation"></i> `);
       return false;
-    }
-    // Cannot submit form if tweet exceeds maximum length"
-    else if ($('.counter').text() < 0) {
+    } else if ($('.counter').text() < 0) { // Cannot submit form if tweet exceeds maximum length"
       $('.validation-error').slideDown("fast");
       $('.validation-error').html(`
       <i class="fa-solid fa-triangle-exclamation"></i> 
       Tweet exceeds the 140 character limit. Summarize your thoughts!
       <i class="fa-solid fa-triangle-exclamation"></i> `);
       return false;
-    }
-    else {
+    } else {
       const tweet = $(this).serialize();
       $.post('/tweets', tweet, () => {
         loadTweets();
         $('#tweet-text').val('');
         $('.counter').text(140);
-      })
+      });
     }
-  })
+  });
   
   const loadTweets = function() {
     $.get('/tweets', function(tweets) {
       renderTweets(tweets);
-    })
+    });
   };
 
   loadTweets();
-
 });
